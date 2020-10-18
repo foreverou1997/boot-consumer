1a28a22ee7aeda019953684ee125e27f78449057
oulei <1306622359@qq.com>
2020-10-18 19:26:11 +0800

    消费者初始化

---
+++ src/main/java/com/study/demo/bootconsumer/BootConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.study.demo.bootconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author oulei
+ */
+@SpringBootApplication
+public class BootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootConsumerApplication.class, args);
+    }
+
+}
